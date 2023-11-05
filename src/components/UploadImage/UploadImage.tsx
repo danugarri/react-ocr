f47281b7d9aa746079bfff3312b227463ae542a1
danugarri <danugarri@hotmail.com>
2023-11-05 15:17:33 +0100

    Handle selectedImage reset

--- src/components/UploadImage/UploadImage.tsx
+++ src/components/UploadImage/UploadImage.tsx
@@ -1,14 +1,23 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import './UploadImage.css';
 
 export type UploadImageProps = {
   selectedImage: File | null;
   handleImageChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
 };
+
 const UploadImage: React.FC<UploadImageProps> = ({ handleImageChange, selectedImage }) => {
+  const fileInputRef = useRef<HTMLInputElement | null>(null);
+
+  const handleSelectImage = () => {
+    if (fileInputRef.current) {
+      fileInputRef.current.value = ''; // Reset the input element
+    }
+  };
+
   return (
     <div>
-      <button className="uploadImage-button">
+      <button className="uploadImage-button" onClick={handleSelectImage}>
         <label htmlFor="input-file" className="uploadImage-label">
           Select an Image
         </label>
@@ -16,6 +25,7 @@ const UploadImage: React.FC<UploadImageProps> = ({ handleImageChange, selectedIm
       <input
         type="file"
         id="input-file"
+        ref={fileInputRef}
         accept="image/*"
         onChange={handleImageChange}
         className="uploadImage-not-displayed"
