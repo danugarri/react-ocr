cf73b883503d61798cb15650776d9794b22dcdb3
danugarri <danugarri@hotmail.com>
2023-11-05 12:29:29 +0100

    Created custom input

--- src/components/OCR/OCR.tsx
+++ src/components/OCR/OCR.tsx
@@ -1,14 +1,16 @@
 import React from 'react';
 import { OCRProps } from './OCR.types';
 import { useOCRHandler } from './hooks/useOCRHandler';
+import ProgressBar from '../ProgressBar/ProgressBar';
+import AlertDialog from '../Dialog/Dialog';
 
 export const OCR: React.FC<OCRProps> = ({ selectedImage }) => {
   const { error, isLoading, text } = useOCRHandler(selectedImage);
 
   if (isLoading) {
-    return <>{'spinner'}</>;
+    return <ProgressBar />;
   } else if (error) {
-    return <>{'Pop-up'}</>;
+    return <AlertDialog error={error} />;
   } else {
     return <>{text}</>;
   }
--- src/components/UploadImage/UploadImage.tsx
+++ src/components/UploadImage/UploadImage.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import './UploadImage.css';
 
 export type UploadImageProps = {
   selectedImage: File | null;
@@ -7,16 +8,29 @@ export type UploadImageProps = {
 const UploadImage: React.FC<UploadImageProps> = ({ handleImageChange, selectedImage }) => {
   return (
     <div>
-      <input type="file" accept="image/*" onChange={handleImageChange} />
-      {selectedImage && (
+      <button className="uploadImage-button">
+        <label htmlFor="input-file" className="uploadImage-label">
+          Select a file
+        </label>
+      </button>
+      <input
+        type="file"
+        id="input-file"
+        accept="image/*"
+        onChange={handleImageChange}
+        className="uploadImage-not-displayed"
+      />
+      {selectedImage ? (
         <div>
           <p>Selected Image: {selectedImage.name}</p>
           <img
             src={URL.createObjectURL(selectedImage)}
             alt="Selected"
-            style={{ width: '80px', height: '80px' }}
+            className="uploadImage-selected-image"
           />
         </div>
+      ) : (
+        <p>No Files selected</p>
       )}
     </div>
   );
