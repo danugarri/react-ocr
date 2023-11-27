59436afd6eb3e9331a13252a8412204759ab8b99
danugarri <danugarri@hotmail.com>
2023-11-27 23:44:26 +0100

    Possible approach for pages in pdf

--- src/components/PdfGenerator/pdfGenerator.ts
+++ src/components/PdfGenerator/pdfGenerator.ts
@@ -6,7 +6,37 @@ export const generatePDF = (text: string) => {
     align: 'left',
     maxWidth: 185,
   };
-  doc.setFont('times', 'italic');
-  doc.text(text, 10, 10, textOptions);
+
+  // Set padding
+  const padding = 5;
+
+  // Set initial position
+  const xPos = 10;
+  let yPos = padding;
+
+  // Set page height and width
+  const pageHeight = doc.internal.pageSize.height - 2 * padding;
+  const pageWidth = doc.internal.pageSize.width - 2 * padding;
+
+  // Split text into lines
+  const lines = doc.splitTextToSize(text, pageWidth - 2 * padding);
+
+  // Loop through lines and add to the PDF
+  lines.forEach((line: string) => {
+    // Check if adding the current line exceeds the page height
+    if (yPos + doc.getTextDimensions(line, textOptions).h > pageHeight) {
+      // Add a new page if the current line exceeds the page height
+      doc.addPage();
+      yPos = padding; // Reset yPos for the new page
+    }
+
+    // Add text to the PDF
+    doc.text(line, xPos, yPos, textOptions);
+
+    // Update yPos for the next line
+    yPos += doc.getTextDimensions(line, textOptions).h + padding;
+  });
+
+  // Save the PDF
   doc.save('OcrPDF.pdf');
 };
