d502e52bd3733379d2884e9b09ba655547cf9deb
danugarri <danugarri@hotmail.com>
2023-11-27 23:48:24 +0100

    Added margin

--- src/components/PdfGenerator/pdfGenerator.ts
+++ src/components/PdfGenerator/pdfGenerator.ts
@@ -7,16 +7,17 @@ export const generatePDF = (text: string) => {
     maxWidth: 185,
   };
 
-  // Set padding
+  // Set margin and padding
+  const margin = 20;
   const padding = 5;
 
   // Set initial position
-  const xPos = 10;
-  let yPos = padding;
+  const xPos = margin;
+  let yPos = margin;
 
   // Set page height and width
-  const pageHeight = doc.internal.pageSize.height - 2 * padding;
-  const pageWidth = doc.internal.pageSize.width - 2 * padding;
+  const pageHeight = doc.internal.pageSize.height - 2 * margin;
+  const pageWidth = doc.internal.pageSize.width - 2 * margin;
 
   // Split text into lines
   const lines = doc.splitTextToSize(text, pageWidth - 2 * padding);
@@ -24,10 +25,10 @@ export const generatePDF = (text: string) => {
   // Loop through lines and add to the PDF
   lines.forEach((line: string) => {
     // Check if adding the current line exceeds the page height
-    if (yPos + doc.getTextDimensions(line, textOptions).h > pageHeight) {
+    if (yPos + doc.getTextDimensions(line, textOptions).h + padding > pageHeight) {
       // Add a new page if the current line exceeds the page height
       doc.addPage();
-      yPos = padding; // Reset yPos for the new page
+      yPos = margin; // Reset yPos for the new page
     }
 
     // Add text to the PDF
