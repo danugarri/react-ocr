874f9e509e26f466f60794c91e0ca75536763696
danugarri <danugarri@hotmail.com>
2023-11-28 21:44:42 +0100

    split code

--- src/components/PdfGenerator/pdfGenerator.consts.ts
+++ src/components/PdfGenerator/pdfGenerator.consts.ts
@@ -1,4 +1,4 @@
-import { TextOptionsLight } from 'jspdf';
+import jsPDF, { TextOptionsLight } from 'jspdf';
 
 export const textOptions: TextOptionsLight = {
   align: 'left',
@@ -11,3 +11,10 @@ export const pageOptions = {
   footerHeight: 15,
   xPos: 10,
 };
+
+export const logoOptions = {
+  imgWidth: 10,
+  imgHeight: 10,
+};
+
+export const hasOnePage = (doc: jsPDF) => doc.internal.pages.length - 1 === 1;
--- src/components/PdfGenerator/pdfGenerator.ts
+++ src/components/PdfGenerator/pdfGenerator.ts
@@ -1,31 +1,29 @@
 import jsPDF from 'jspdf';
 import logo from '../../assets/images/favicon.png';
-import { textOptions, pageOptions } from './pdfGenerator.consts';
+import { textOptions, pageOptions, hasOnePage, logoOptions } from './pdfGenerator.consts';
 import { addFooter } from './pdfGenerator.helpers';
 
 const { footerHeight, margin, padding, xPos } = pageOptions;
+const { imgHeight, imgWidth } = logoOptions;
 export const generatePDF = (text: string) => {
   const doc = new jsPDF();
   doc.setFont('times', 'roman');
   let yPos = margin;
+  const lines = text.split('\n');
 
-  const addPage = () => {
-    doc.addPage();
-    yPos = margin; // Reset yPos for the new page
-    addFooter(doc); // Add the footer on each new page
-  };
+  // Loop through lines and add line to the PDF
+  lines.forEach((line: string) => {
+    const yAxisHeigth = yPos + doc.getTextDimensions(line, textOptions).h + padding;
+    const contentHeigth = doc.internal.pageSize.height - 2 * margin - footerHeight;
 
-  // Loop through lines and add to the PDF
-  text.split('\n').forEach((line: string) => {
     // reset font-size
     doc.setFontSize(14);
     // Check if adding the current line exceeds the page height
-    if (
-      yPos + doc.getTextDimensions(line, textOptions).h + padding >
-      doc.internal.pageSize.height - 2 * margin - footerHeight
-    ) {
-      addPage();
-    } else if (doc.internal.pages.length - 1 === 1) {
+    if (yAxisHeigth > contentHeigth) {
+      doc.addPage();
+      addFooter(doc);
+      yPos = margin; // Reset yPos for the new page
+    } else if (hasOnePage(doc)) {
       addFooter(doc);
     }
     // Add text to the PDF
@@ -33,9 +31,6 @@ export const generatePDF = (text: string) => {
 
     // Update yPos for the next line
     yPos += doc.getTextDimensions(line, textOptions).h + padding;
-
-    const imgWidth = 10;
-    const imgHeight = 10;
 
     // Add image at the bottom of the current page
     doc.addImage(
